e2de918f70ca1469e0245b7db7c1de22587dee06
cruzguillermo <guillermobelizcruz98@gmail.com>
2023-03-11 01:22:43 -0300

    changes

--- src/components/Contenido/Contenido.jsx
+++ src/components/Contenido/Contenido.jsx
@@ -5,13 +5,18 @@ import WikipediaSearch from "../WikipediaSearch/WikipediaSearch";
 
 const theme = {
   background: "#f5f8fb",
-  headerBgColor: "#eb3449",
+  headerBgColor: "#128C7E",
   headerFontColor: "#fff",
   headerFontSize: "20px",
-  botBubbleColor: "#eb3449",
+  botBubbleColor: "#128C7E",
   botFontColor: "#fff",
   userBubbleColor: "#0cb3c9",
   userFontColor: "#fff",
+  contentStyle: {
+    width: "100%",
+    height: "100%",
+    overflow: "auto",
+  },
 };
 
 export default class Contenido extends Component {
@@ -61,29 +66,30 @@ export default class Contenido extends Component {
               id: "5",
               placeholder: "Enviar mensaje...",
               options: [
-                { value: "Comida", label: "Comida", trigger: "Comida" },
+                { value: "Comida", label: "Comida", trigger: "Comida" ,   placeholder: "Enviar mensaje...",},
                 {
                   value: "Desayunos",
                   label: "Desayunos",
-                  trigger: "Desayunos",
+                  trigger: "Desayunos",   placeholder: "Enviar mensaje...",
                 },
-                { value: "Bebidas", label: "Bebidas", trigger: "Bebidas" },
-                { value: "Postres", label: "Postres", trigger: "Postres" },
+                { value: "Bebidas", label: "Bebidas", trigger: "Bebidas",   placeholder: "Enviar mensaje...", },
+                { value: "Postres", label: "Postres", trigger: "Postres",   placeholder: "Enviar mensaje...", },
                 {
                   value: "Promociones",
                   label: "Promociones",
                   trigger: "Promociones",
+                  placeholder: "Enviar mensaje...",
                 },
               ],
             },
             {
               id: "Comida",
               options: [
-                { value: "Pizzas", label: "Pizzas", trigger: "Pizzas" },
-                {value: "Lomitos", label: "Lomitos", trigger: "Lomitos"},
-                {value: "Hamburguesas", label: "Hamburguesas", trigger: "Hamburguesas"},
-                {value: "Sandwiches", label: "Sandwiches", trigger: "Sandwiches"},
-                {value: "Empanadas", label: "Empanadas", trigger: "Empanadas"}
+                { value: "Pizzas", label: "Pizzas", trigger: "Pizzas",   placeholder: "Enviar mensaje...", },
+                {value: "Lomitos", label: "Lomitos", trigger: "Lomitos" ,   placeholder: "Enviar mensaje...",},
+                {value: "Hamburguesas", label: "Hamburguesas", trigger: "Hamburguesas" ,   placeholder: "Enviar mensaje...",},
+                {value: "Sandwiches", label: "Sandwiches", trigger: "Sandwiches",   placeholder: "Enviar mensaje...", },
+                {value: "Empanadas", label: "Empanadas", trigger: "Empanadas",   placeholder: "Enviar mensaje...", },
               ],
               placeholder: "Enviar mensaje...",
             },
@@ -91,126 +97,149 @@ export default class Contenido extends Component {
               id: "Pizzas",
               message: "Pizza Comun $ 250",
               trigger: "pizzaespecial",
+              placeholder: "Enviar mensaje...",
             },
             {
               id: "pizzaespecial",
               message: "Pizza Especial $ 300",
               trigger: "pizzafugazzeta",
+              placeholder: "Enviar mensaje...",
             },
             {
               id: "pizzafugazzeta",
               message: "Pizza Fugazzeta $ 350",
               trigger: "pizzacalabresa",
+              placeholder: "Enviar mensaje...",
             },
             {
               id: "pizzacalabresa",
               message: "Pizza Calabresa $ 400",
               trigger: "pizzamuzzarella",
+              placeholder: "Enviar mensaje...",
             },
             {
               id: "pizzamuzzarella",
               message: "Pizza Muzzarella $ 450",
               trigger: "pizzanapolitana",
+              placeholder: "Enviar mensaje...",
             },
             {
               id: "pizzanapolitana",
               message: "Pizza Napolitana $ 500",
               trigger: "opcionsiono",
+              placeholder: "Enviar mensaje...",
             },
             {
               id: "opcionsiono",
               message: "Desea ver otra opcion?",
               trigger: "opciones si o no",
+              placeholder: "Enviar mensaje...",
             },
              ////////////////////////////// selections lomitos //////////////////////////////
             {
                 id: "Lomitos",
                 message: "Lomito Comun $ 250",
                 trigger: "lomitoespecial",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "lomitoespecial",
                 message: "Lomito Especial $ 300",
                 trigger: "lomitoarabe",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "lomitoarabe",
                 message: "Lomito Arabe $ 350",
                 trigger: "opcionsiono",
+                placeholder: "Enviar mensaje...",
             },
             ////////////////////////////// selections hamburguesas //////////////////////////////
             {
                 id: "Hamburguesas",
                 message: "Hamburguesa Simple $ 250",
                 trigger: "hamburguesadoble",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "hamburguesadoble",
                 message: "Hamburguesa Doble $ 300",
                 trigger: "hamburguesatriple",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "hamburguesatriple",
                 message: "Hamburguesa Triple $ 350",
-                trigger: "hamburguesabeicon"
+                trigger: "hamburguesabeicon",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "hamburguesabeicon",
                 message: "Hamburguesa con Beicon $ 350",
                 trigger: "opcionsiono",
+                placeholder: "Enviar mensaje...",
             },
             ////////////////////////////// selections sandwiches //////////////////////////////
             {
                 id: "Sandwiches",
                 message: "Sandwich Comun $ 250",
                 trigger: "sandwichespecial",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "sandwichespecial",
                 message: "Sandwich Especial $ 300",
                 trigger: "sandwicharabe",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "sandwicharabe",
                 message: "Sandwich Pan Arabe $ 350",
                 trigger: "sandwichhojaldre",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "sandwichhojaldre",
                 message: "Sandwich Pan Hojaldre $ 350",
                 trigger: "opcionsiono",
+                placeholder: "Enviar mensaje...",
             },
             ////////////////////////////// selections empanadas //////////////////////////////
             {
                 id: "Empanadas",
                 message: "Empanadas Carne $ 250",
                 trigger: "empanadapollo",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "empanadapollo",
                 message: "Empanadas Pollo $ 300",
                 trigger: "empanadajamonyqueso",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "empanadajamonyqueso",
                 message: "Empanadas Jamon y Queso $ 350",
                 trigger: "empanadaarabes",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "empanadaarabes",
                 message: "Empanadas Arabes $ 350",
                 trigger: "empanadaternera",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "empanadaternera",
                 message: "Empanadas Ternera $ 350",
                 trigger: "opcionsiono",
+                placeholder: "Enviar mensaje...",
             },
             {
               id: "opciones si o no",
               options: [
-                { value: "si", label: "Si", trigger: "5" },
-                { value: "no", label: "No", trigger: "FinDePedido" },
+                { value: "si", label: "Si", trigger: "5", placeholder: "Enviar mensaje...", },
+                { value: "no", label: "No", trigger: "FinDePedido", placeholder: "Enviar mensaje...", },
               ],
             },
             ////////////////////////////// selections desayunos //////////////////////////////
@@ -224,39 +253,45 @@ export default class Contenido extends Component {
                 id: "d1",
                 message: "Cafe grande $ 150",
                 trigger: "d2",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "d2",
                 message: "cafe,te o matecocido con medialunas $ 200",
                 trigger: "d3",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "d3",
                 message: "cafe,te o matecocido con medialunas de jamon y queso $ 250",
                 trigger: "d4",
+                placeholder: "Enviar mensaje...",
             }, 
             {
                 id: "d4",
                 message: "cafe,te o matecocido con porcion de torta $ 550",
                 trigger: "d5",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "d5",
                 message: "cafe,te o matecocido con tostadas, queso crema, mermelada y ensalada de frutas $ 650",
                 trigger: "d6",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "d6",
                 message: "cafe,te o matecocido con tostadas, queso crema, mermelada y ensalada de frutas $ 650",
                 trigger: "opcionsiono",
+                placeholder: "Enviar mensaje...",
             },
             ////////////////////////////// selections bebidas //////////////////////////////
             {
               id: "Bebidas",
               options: [
-                { value: "Licuados", label: "Licuados", trigger: "Licuados" },
-                { value: "Gaseosas", label: "Gaseosas", trigger: "Gaseosas" },
-                { value: "Cervezas", label: "Cervezas", trigger: "Cervezas" },
+                { value: "Licuados", label: "Licuados", trigger: "Licuados", placeholder: "Enviar mensaje...", },
+                { value: "Gaseosas", label: "Gaseosas", trigger: "Gaseosas" , placeholder: "Enviar mensaje...", },
+                { value: "Cervezas", label: "Cervezas", trigger: "Cervezas" , placeholder: "Enviar mensaje...", },
                 ],
               placeholder: "Enviar mensaje...",
             },
@@ -265,78 +300,93 @@ export default class Contenido extends Component {
                 id: "Licuados",
                 message: "Licuado de frutas $ 150",
                 trigger: "l1",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "l1",
                 message: "durazno $ 200",
                 trigger: "l2",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "l2",
                 message: "frutilla $ 200",
                 trigger: "l3",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "l3",
                 message: "naranja $ 200",
                 trigger: "l4",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "l4",
                 message: "banana $ 200",
                 trigger: "opcionsiono",
+                placeholder: "Enviar mensaje...",
             },
             ////////////////////////////// selections Gaseosas //////////////////////////////
             {
                 id: "Gaseosas",
                 message: "Coca Cola $ 100",
                 trigger: "g1",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "g1",
                 message: "Sprite $ 100",
                 trigger: "g2",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "g2",
                 message: "Fanta $ 100",
                 trigger: "g3",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "g3",
                 message: "7up $ 100",
                 trigger: "g4",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "g4",
                 message: "Manaos $ 100",
                 trigger: "opcionsiono",
+                placeholder: "Enviar mensaje...",
             },
             ////////////////////////////// selections Cervezas //////////////////////////////
             {
                 id: "Cervezas",
                 message: "Quilmes $ 100",
                 trigger: "c1",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "c1",
                 message: "Patagonia $ 100",
                 trigger: "c2",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "c2",
                 message: "Stella Artois $ 100",
                 trigger: "c3",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "c3",
                 message: "Corona $ 100",
                 trigger: "c4",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "c4",
                 message: "Brahma $ 100",
                 trigger: "opcionsiono",
+                placeholder: "Enviar mensaje...",
             },
             ////////////////////////////// selections postres //////////////////////////////
             {
@@ -348,20 +398,24 @@ export default class Contenido extends Component {
                 id: "p1",
                 message: "Torta de frutilla $ 350",
                 trigger: "p2",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "p2",
                 message: "Torta de durazno $ 350",
                 trigger: "p3",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "p3",
                 message: "Torta de banana $ 350",
                 trigger: "p4",
+                placeholder: "Enviar mensaje...",
             },{
                 id: "p4",
                 message: "Torta de naranja $ 350",
                 trigger: "opcionsiono",
+                placeholder: "Enviar mensaje...",
             },
             ////////////////////////////// selections promociones //////////////////////////////
             {
@@ -374,21 +428,25 @@ export default class Contenido extends Component {
                 id: "pr1",
                 message: "Promocion 2: 2x1 en tortas",
                 trigger: "pr2",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "pr2",
                 message: "Promocion 3: 2x1 en medialunas",
                 trigger: "pr3",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "pr3",
                 message: "Promocion 4: 2x1 en tostadas",
                 trigger: "pr4",
+                placeholder: "Enviar mensaje...",
             },
             {
                 id: "pr4",
                 message: "Promocion 5: 2x1 en medialunas de jamon y queso",
                 trigger: "opcionsiono",
+                placeholder: "Enviar mensaje...",
             },
             {
               id: "FinDePedido",
@@ -396,25 +454,6 @@ export default class Contenido extends Component {
               end: true,
               placeholder: "Enviar mensaje...",
             },
-
-            // {
-            //     id: "9",
-            //     component: <WikipediaSearch />,
-            //     asMessage: true,
-            //     trigger: "preguntaVuelta"
-            // },
-            // {
-            //     id: "preguntaVuelta",
-            //     message: "Do you need to know anything else?",
-            //     trigger: "respuestaVuelta",
-            // },
-            // {
-            //     id: "respuestaVuelta",
-            //     options: [
-            //         {value: "y", label: "Yes", trigger: "6A"},
-            //         {value: "n", label: "No", trigger: "6B"},
-            //     ],
-            // }
           ]}
         />
       </ThemeProvider>
